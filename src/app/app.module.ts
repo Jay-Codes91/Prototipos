34c4f62b55e3e500680b02c875f67ca50d51ac2c
Jay <yhgg@gmail.com>
2022-03-29 23:41:29 -0600

    plantilla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,34 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {FormsModule} from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import {InputTextModule} from 'primeng-lts/inputtext';
+import {TabViewModule} from 'primeng-lts/tabview';
+import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { AdminComponent } from './Components/admin/admin.component';
+
+const routes: Routes = [
+   {path: '', component: LoginComponent, pathMatch: 'full'},
+   {path: 'admin', component: AdminComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    InputTextModule
+    InputTextModule,
+    TabViewModule,
+    NgxMaterialTimepickerModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent],
